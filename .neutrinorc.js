eb3e93b1c694afab7910bd509283a48dbceccbf9
Vladimir Kalinichev <wrumly@gmail.com>
2019-01-22 15:12:49 +0300

    Transpile to es5

--- .neutrinorc.js
+++ .neutrinorc.js
@@ -1,6 +1,24 @@
+const { merge } = require('@neutrinojs/compile-loader');
+
 module.exports = {
   use: [
     '@neutrinojs/standardjs',
     '@neutrinojs/react-components',
+    neutrino => {
+      neutrino.config.module
+        .rule('compile')
+        .use('babel')
+        .tap(options => merge(options, {
+          presets: [
+            ['babel-preset-env', {
+              targets: {
+                ie: 9
+              },
+              useBuiltIns: false,
+              modules: false
+            }]
+          ]
+        }));
+    }
   ]
 };
